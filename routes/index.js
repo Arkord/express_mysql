dd5a5e1081aca907327621743e9bdd65ed98ff54
Octavio Mejia <arkord1@gmail.com>
2023-03-07 15:08:30 -0600

    mysql

---
+++ routes/index.js
@@ -0,0 +1,9 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+    res.render('index', { title: 'Express', message: 'The students of 5B are smarter than 5A or no' });
+});
+
+module.exports = router;
